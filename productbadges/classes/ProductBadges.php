09449360e1556793642e0b3534e6e6df741318fd
cristian <cr.m23@hotmail.com>
2026-05-14 20:24:25 +0200

    implemented tags entity

---
+++ productbadges/classes/ProductBadges.php
@@ -0,0 +1,47 @@
+<?php
+
+class Badge extends ObjectModel
+{
+    public $id_badge;
+    public $name;
+    public $background_color;
+    public $text_color;
+    public $position;
+    public $active;
+
+    public static $definition = [
+        'table' => 'badge',
+        'primary' => 'id_badge',
+        'fields' => [
+            'name' => [
+                'type' => self::TYPE_STRING,
+                'validate' => 'isGenericName',
+                'required' => true,
+                'size' => 50
+            ],
+            'background_color' => [
+                'type' => self::TYPE_STRING,
+                'validate' => 'isGenericName',
+                'required' => true,
+                'size' => 10
+            ],
+            'text_color' => [
+                'type' => self::TYPE_STRING,
+                'validate' => 'isGenericName',
+                'required' => true,
+                'size' => 10
+            ],
+            'position' => [
+                'type' => self::TYPE_STRING,
+                'validate' => 'isGenericName',
+                'required' => true,
+                'size' => 20
+            ],
+            'active' => [
+                'type' => self::TYPE_BOOL,
+                'validate' => 'isBool',
+                'required' => true
+            ]
+        ]
+    ];
+}
